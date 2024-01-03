a125708d7ea64b11dba372fb914fd582f42ec41a
uniquesreedhar <97074135+uniquesreedhar@users.noreply.github.com>
2024-01-03 16:23:56 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sreedhar8897/python-flask-app:latest
+docker pull sreedhar8897/simple-flask-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 5001:5000 sreedhar8897/python-flask-app
+docker run -dit -p 5001:5004 sreedhar8897/simple-flask-app
